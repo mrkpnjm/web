317179d5471efe0a687fad4c648efcc2407b6148
jorgensinka <jorgensinka@gmail.com>
2026-05-30 02:25:30 +0300

    Added style - padding

--- frontend/src/components/MainLayout.tsx
+++ frontend/src/components/MainLayout.tsx
@@ -5,7 +5,7 @@ export const MainLayout = () => {
     return (
         <div className="bg-body min-vh-100 d-flex flex-column">
             <Navbar />
-            <main className="mt-5 pt-3 flex-grow-1">
+            <main className="mt-5 pt-2 flex-grow-1" style={{ paddingTop: '60px' }}>
                 <div className="w-100">
                     <Outlet />
                 </div>
